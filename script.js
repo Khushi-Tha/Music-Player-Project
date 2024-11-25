29ac5c137b68320db17ef89acae65c35f0024f68
Khushi-Tha <157682511+Khushi-Tha@users.noreply.github.com>
2024-11-25 21:40:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,25 +25,25 @@ const music_list = [
         img : 'images/stay.png',
         name : 'Stay',
         artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        music : 'stay.mp3'
     },
     {
         img : 'images/fallingdown.jpg',
         name : 'Falling Down',
         artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+        music : 'fallingdown.mp3'
     },
     {
         img : 'images/faded.png',
         name : 'Faded',
         artist : 'Alan Walker',
-        music : 'music/music_Faded.mp3'
+        music : 'music_Faded.mp3'
     },
     {
         img : 'images/ratherbe.jpg',
         name : 'Rather Be',
         artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
+        music : 'Rather Be.mp3'
     }
 ];
 
